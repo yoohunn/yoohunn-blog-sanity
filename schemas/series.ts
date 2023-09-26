016557678518941c1ac8f414dd62b641f3edbe2d
YooHyunJ <poirtmove@gmail.com>
2023-09-26 11:24:13 +0900

    feat: series slug 필드 추가

--- schemas/series.ts
+++ schemas/series.ts
@@ -12,6 +12,16 @@ export default defineType({
       validation: (Rule) => Rule.required(),
     }),
     defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
       name: 'description',
       title: 'Description',
       type: 'text',
