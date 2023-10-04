71125e36ee1d5f833066342d300fe9ea12cef129
YooHyunJ <poirtmove@gmail.com>
2023-10-04 15:27:43 +0900

    feat: author descirption 필드 추가

--- schemas/author.ts
+++ schemas/author.ts
@@ -12,6 +12,12 @@ export default defineType({
       validation: (Rule) => Rule.required(),
     }),
     defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
       name: 'slug',
       title: 'Slug',
       type: 'slug',
